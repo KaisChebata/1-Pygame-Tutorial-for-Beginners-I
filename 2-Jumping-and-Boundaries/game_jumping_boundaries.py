51fa1b667f3818f8af63418156f64f98e0d25f97
Kais Chebata <qays.sh@gmail.com>
2021-01-26 20:30:46 +0100

    adding jumping formula

--- 2-Jumping-and-Boundaries/game_jumping_boundaries.py
+++ 2-Jumping-and-Boundaries/game_jumping_boundaries.py
@@ -14,6 +14,10 @@ x, y = 50, 425
 width, height = 40, 60
 vel = 5
 
+# set jumping state
+is_jump = False
+jump_count = 10
+
 # set up main loop
 run = True
 
@@ -31,10 +35,23 @@ while run:
         x -= vel
     if keys[pygame.K_RIGHT] and x < screen_width - width - vel:
         x += vel
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < screen_height - height - vel:
-        y += vel
+    if not is_jump:
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < screen_height - height - vel:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            is_jump = True
+    else:
+        if jump_count >= -10:
+            neg = 1
+            if jump_count < 0:
+                neg = -1
+            y -= (jump_count ** 2) * 0.5 * neg
+            jump_count -= 1
+        else:
+            is_jump = False
+            jump_count = 10
     
     window.fill((0, 0, 0))
     pygame.draw.rect(window, (255, 0, 0), (x, y, width, height))
