75225067dfeaa099a12186e38afc288a03ec36ad
Kais Chebata <qays.sh@gmail.com>
2021-02-03 18:48:44 +0100

    add settings.py module that contains Settings class to store game's settings

---
+++ 4-Optimization-OOP/settings.py
@@ -0,0 +1,11 @@
+
+class Settings:
+    """A Class to store all settings for Super Sprite Game."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        #Screen Settings
+
+        self.screen_width = 500
+        self.screen_height = 480
+        self.background_path = 'raw_media/bg.jpg'
