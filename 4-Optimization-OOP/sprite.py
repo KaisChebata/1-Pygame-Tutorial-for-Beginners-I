54a3e8d532ba78c30ae7b97e95f4b6e333942721
Kais Chebata <qays.sh@gmail.com>
2021-02-10 13:38:32 +0100

    modify update method to limit the Sprite's range

--- 4-Optimization-OOP/sprite.py
+++ 4-Optimization-OOP/sprite.py
@@ -54,9 +54,9 @@ class Player:
     
     def update(self):
         """Update the sprite's position based on the movement flags"""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.window_rect.right:
             self.x += self.settings.sprite_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.sprite_speed
         
         # update rect object from self.x
