e930f2242d6328da09896fa3819384e1701bc29c
Kais Chebata <qays.sh@gmail.com>
2021-03-17 15:16:50 +0100

    moving rects with mouse

---
+++ work_with_rects_demos/move_rect_with_mouse.py
@@ -0,0 +1,54 @@
+import pygame
+from pygame.locals import *
+
+"""
+- The function rect.collidepoint(pos) returns True 
+  if the point collides with the rectangle.
+- we move the rectangle by the relative motion of the mouse event.rel.
+
+- The boolean variable `moving` is set when the mouse button 
+  goes down inside the rectangle. 
+  It remains True until the button goes up again. 
+  The rectangle is only moved when the mouse click has happened 
+  inside the rectangle. 
+  While the rectangle is moving, we add a blue outline.
+"""
+
+width, height = 500, 200
+SIZE = (width, height)
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GRAY = (150, 150, 150)
+
+pygame.init()
+
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption('Move rect with mouse')
+
+rect = Rect(50, 60, 200, 80)
+moving = False
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        
+        elif event.type == MOUSEBUTTONDOWN:
+            if rect.collidepoint(event.pos):
+                moving = True
+        
+        elif event.type == MOUSEBUTTONUP:
+            moving = False
+        
+        elif event.type == MOUSEMOTION and moving:
+            rect.move_ip(event.rel)
+    
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, RED, rect)
+    if moving:
+        pygame.draw.rect(screen, BLUE, rect, 4)
+    pygame.display.flip()
+
+pygame.quit()
