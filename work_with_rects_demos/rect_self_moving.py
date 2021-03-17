a804543e2a0cddb197ed7c0fcd8d53e283112c0d
Kais Chebata <qays.sh@gmail.com>
2021-03-17 16:18:30 +0100

    Self moving rect demo

---
+++ work_with_rects_demos/rect_self_moving.py
@@ -0,0 +1,38 @@
+from rect import *
+
+"""
+The following code moves a rectangle by the amount v:
+
+rect.move_ip(v)
+
+It then checks the 4 borders and inverts the speed component 
+if the rectangle is outside of the application window.
+"""
+
+rect = Rect(100, 50, 50, 50)
+v = [2, 2]
+
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        
+    rect.move_ip(v)
+    
+    if rect.left < 0:
+        v[0] *= -1
+    
+    if rect.right > width:
+        v[0] *= -1
+    
+    if rect.top < 0:
+        v[1] *= -1
+    
+    if rect.bottom > height:
+        v[1] *= -1
+    
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, RED, rect)
+    pygame.display.flip()
+
+pygame.quit()
