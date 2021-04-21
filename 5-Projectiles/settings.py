a1256173a525b31ed7189f397d84ab7d391d166b
Kais Chebata <qays.sh@gmail.com>
2021-04-22 00:00:28 +0100

    add projectile settings

--- 5-Projectiles/settings.py
+++ 5-Projectiles/settings.py
@@ -14,3 +14,8 @@ class Settings:
         # Player Settings
         self.sprite_speed = 5
         
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
